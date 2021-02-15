8552939a49423457452c569269ec94cb554b4128
VladimirKornev <vladroot@gmail.com>
2021-02-15 19:40:44 +0300

    async tasks update.

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
+from asyncio import sleep
 import os
 import json
 import discord
-from time import sleep
 from datetime import datetime
 from discord.abc import GuildChannel
 from discord.client import Client
@@ -48,42 +48,71 @@ class CustomClient(Client):
                 for channel in self._server.channels:
                     if channel.name == 'general':
                         self._generalChannel = channel
-
                 break
 
         self._members = self._server.members
         members = '\n - '.join([member.name for member in self._members])
         print(f'Guild Members:\n - {members}')
 
-        await self._timed_events()
+        self.loop.create_task(timed_events(self))
 
     async def on_member_join(self, member):
         print(f'{member.name} joined')
         self._members.append(member)
 
-    async def _timed_events(self):
-        while True:
-            currentTime = datetime.utcnow()
-            dayName = get_day_name(currentTime.weekday())
-            if dayName in meetings:
-                await self._process_work_day(dayName, currentTime)
-                sleep(10)
-            else:
-                sleep(600)
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
 
-    async def _process_work_day(self, dayName: str, currTime: datetime):
-        events = [evnt for evnt in meetings[dayName] if evnt['hour'] == currTime.hour and
-                  evnt['minute'] == currTime.minute and currTime.second <= 10]
-        if len(events) > 0:
-            print(f'Event started {currTime}')
-            await self._generalChannel.send(content=f'@everyone Daily meeting started!')
-            sleep(5)
+        msg_content: str = message.content
+        if not msg_content.startswith('!'):
+            return
+
+        command: str = msg_content.removeprefix('!')
+        data: str = ''
+        if command == 'week':
+            data = meetings
+            if len(data) == 0:
+                data = 'No meetings today.'
+        elif command == 'today':
+            data = meetings[get_day_name(datetime.utcnow().weekday())]
+            if len(data) == 0:
+                data = 'No meetings today.'
+        elif command.startswith('schedule'):
+            to_add: str = command.removeprefix('schedule').lstrip()
+            print(str)
+        else:
+            data = 'I don\'t know this trick :('
+        await message.channel.send(data)
+
+    async def SendToGeneral(self, text: str):
+        await self._generalChannel.send(text)
+
+
+async def timed_events(botClient: CustomClient):
+    while True:
+        currentTime = datetime.utcnow()
+        dayName = get_day_name(currentTime.weekday())
+        if dayName in meetings:
+            await process_work_day(botClient, dayName, currentTime)
+            await sleep(10)
+        else:
+            await sleep(600)
+
+
+async def process_work_day(botClient: CustomClient, dayName: str, currTime: datetime):
+    events = [evnt for evnt in meetings[dayName] if evnt['hour'] == currTime.hour and
+              evnt['minute'] == currTime.minute and currTime.second <= 10]
+    if len(events) > 0:
+        print(f'Event started {currTime}')
+        await botClient.SendToGeneral(content=f'@everyone Daily meeting started!')
+        sleep(1)
 
 
 try:
     myIntents = discord.Intents.default()
     myIntents.members = True
-    myIntents.presences = False
+    myIntents.presences = True
 
     client = CustomClient(intents=myIntents)
     client.run(botToken)
